c44c7358c664d2c3881e8f6b6f8f3747f8db971e
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2026-01-22 15:16:19 +0100

    fix: contract-interaction

--- near-api-js/examples/contract-interaction.js
+++ near-api-js/examples/contract-interaction.js
@@ -1,45 +1,32 @@
-import { Account, JsonRpcProvider } from "near-api-js";
+import dotenv from "dotenv";
+import { Account, JsonRpcProvider, teraToGas } from "near-api-js";
+
+// Create an account object
+dotenv.config();
+const privateKey = process.env.PRIVATE_KEY;
+const accountId = process.env.ACCOUNT_ID;
 
 // Create a testnet provider
 const provider = new JsonRpcProvider({
   url: "https://test.rpc.fastnear.com",
 });
 
-// Create an account object
-const privateKey = "ed25519:5nMxVjR3idXu9TiKLw69SxAfoBuHuJJ2uozamfwsrdoKBwmkXTJWf9LDrrwbJ7nfLb8e8Ja7AtUihYbypATWe2iw";
-const accountId = "gain-adult-structure.testnet";
-const account = new Account(accountId, provider, privateKey); // example-account.testnet
-
-// First call, with ecdsa signature
-const ecdsa = await account.callFunction({
-  contractId: 'v1.signer-prod.testnet',
-  methodName: "sign",
-  args: {
-    request: {
-      payload_v2: { 'Ecdsa': '5f3f680429798f95c31a6176ed29322539573459560affee1ac39944a90dd191' },
-      path: 'test',
-      domain_id: 0
-    }
-  },
-  deposit: '1',
-  gas: '30000000000000',
+// For read only calls, you can use the provider directly
+const messages = await provider.callFunction({
+  contractId: 'guestbook.near-examples.testnet',
+  method: "get_messages",
+  args: {},
 });
 
-console.log({ ecdsa });
+console.log(messages);
 
-// Second call, with eddsa signature
-const eddsa = await account.callFunction({
-  contractId: 'v1.signer-prod.testnet',
-  methodName: "sign",
-  args: {
-    request: {
-      payload_v2: { "Eddsa": "01000103f4c2920c126179e614afd2dcfddf71f31d32267198bbfbeedf49e6fdf393cb0cdfedba60c362cbfa11805184949096d8094686429b80c44717ebe71928e51f0b0000000000000000000000000000000000000000000000000000000000000000b67870443c1f6737846e916a7209ad656d9f218d0daff513d4caaadb2ef6ce4401020200010c0200000000ca9a3b00000000" },
-      path: 'test',
-      domain_id: 1
-    }
-  },
-  deposit: '1',
-  gas: '30000000000000',
+// To modify state, you need an account to sign the transaction
+const account = new Account(accountId, provider, privateKey);
+
+// Call the contract
+await account.callFunction({
+  contractId: 'guestbook.near-examples.testnet',
+  methodName: "add_message",
+  args: { text: "Hello!" },
+  gas: teraToGas('30'),
 });
-
-console.log({ eddsa });
