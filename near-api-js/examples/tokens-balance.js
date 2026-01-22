6b4eec1647566e35fb6ee83e38a2344a631a8a05
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2026-01-22 14:03:07 +0100

    chore: simplified example

--- near-api-js/examples/tokens-balance.js
+++ near-api-js/examples/tokens-balance.js
@@ -1,17 +1,6 @@
-import { Account, JsonRpcProvider, FungibleToken } from "near-api-js";
-import { NEAR } from "near-api-js/tokens";
+import { Account, JsonRpcProvider } from "near-api-js";
+import { NEAR, FungibleToken } from "near-api-js/tokens";
 import { USDT } from "near-api-js/tokens/testnet";
-
-import dotenv from "dotenv";
-
-const REF = new FungibleToken("ref.fakes.testnet", {
-  decimals: 18,
-  symbol: "REF",
-});
-
-// Load environment variables
-dotenv.config();
-const accountId = process.env.ACCOUNT_ID;
 
 // Create a connection to testnet RPC
 const provider = new JsonRpcProvider({
@@ -19,19 +8,22 @@ const provider = new JsonRpcProvider({
 });
 
 // Create an account object
+const accountId = 'influencer.testnet';
 const account = new Account(accountId, provider);
 
-// ------- Fetch NEAR tokens balance in the smallest units as BigInt -------
+// ------- Fetch NEAR tokens balance -------
 const nearTokensBalanceInt = await account.getBalance(NEAR);
-console.log("NEAR as units: ", nearTokensBalanceInt);
-console.log("NEAR: ", NEAR.toDecimal(nearTokensBalanceInt));
+console.log("NEAR: ", NEAR.toDecimal(nearTokensBalanceInt, 2));
 
-// ------- Fetch USDT tokens balance in the smallest units as BigInt -------
+// ------- Fetch USDT tokens balance -------
 const usdtTokensBalanceInt = await account.getBalance(USDT);
-console.log("USDT as units: ", usdtTokensBalanceInt);
-console.log("USDT: ", USDT.toDecimal(usdtTokensBalanceInt));
+console.log("USDT: ", USDT.toDecimal(usdtTokensBalanceInt, 2));
 
 // ------- Fetch REF tokens balance in the smallest units as BigInt -------
+const REF = new FungibleToken("ref.fakes.testnet", {
+  decimals: 18,
+  symbol: "REF",
+});
+
 const refTokensBalanceInt = await account.getBalance(REF);
-console.log("REF as units: ", refTokensBalanceInt);
-console.log("REF: ", REF.toDecimal(refTokensBalanceInt));
+console.log("REF: ", REF.toDecimal(refTokensBalanceInt, 2));
