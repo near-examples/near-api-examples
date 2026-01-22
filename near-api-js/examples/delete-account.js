ea79cf927d63c8bf9300f28ce2811ebb0c9fedf8
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2026-01-22 14:48:46 +0100

    fix: delete-account example

--- near-api-js/examples/delete-account.js
+++ near-api-js/examples/delete-account.js
@@ -1,14 +1,11 @@
+import dotenv from "dotenv";
 import { Account, JsonRpcProvider, KeyPairSigner, KeyPair } from "near-api-js";
 import { NEAR } from "near-api-js/tokens";
 
-import dotenv from "dotenv";
-
 dotenv.config();
-
-// Create a signer from a private key string
+const accountId = process.env.ACCOUNT_ID;
 const privateKey = process.env.PRIVATE_KEY;
 const keyPair = KeyPair.fromString(privateKey);
-const signer = KeyPairSigner.fromSecretKey(privateKey); // ed25519:5Fg2...
 
 // Create a connection to testnet RPC
 const provider = new JsonRpcProvider({
@@ -16,19 +13,18 @@ const provider = new JsonRpcProvider({
 });
 
 // Create a new account to be deleted
-const accountId = process.env.ACCOUNT_ID;
-const master = new Account(accountId, provider, signer)
+const master = new Account(accountId, provider, privateKey)
 
 const deleteMe = `${Date.now()}.testnet`;
 
-await master.createTopLevelAccount(
-  deleteMe,
-  keyPair.getPublicKey().toString(),
-  NEAR.toUnits("0.1")
-);
+await master.createAccount({
+  newAccountId: deleteMe,
+  publicKey: keyPair.getPublicKey().toString(),
+  nearToTransfer: NEAR.toUnits("0.1")
+});
 
 // Create an account object for the new account with corresponding signer
-const accountToDelete = new Account(deleteMe, provider, signer);
+const accountToDelete = new Account(deleteMe, provider, privateKey);
 
 // Delete the account 
 await accountToDelete.deleteAccount(accountId); // example-beneficiary.testnet
